919c18f0e42e602be3071cfa126c7c544b684143
din jakupi <din.jakupi@gmail.com>
2020-06-20 17:49:48 +0200

    created class to handle datetimes

---
+++ date/dateHandler.py
@@ -0,0 +1,16 @@
+from datetime import date
+import datetime
+
+class dateHandler():
+    
+    def getDate(self):
+        yesterday = date.now() - datetime.timedelta(days=1)        
+        end_yesterday = yesterday + datetime.timedelta(hours=23, minutes=59)
+        string_date, string_datetime = self.dateToString(yesterday)        
+        return yesterday, end_yesterday, string_date, string_datetime
+    
+    def dateToString(self, yesterday):
+        string_date = datetime.datetime.strftime(yesterday,"%Y-%m-%d")
+        string_datetime = datetime.datetime.strftime(yesterday, "%Y-%m-%d %H:%M:%S")
+        return string_date, string_datetime
+    
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from database.database import database
+from date.dateHandler import dateHandler
 
 db = database()
+dateHandler = dateHandler()
+
+#date variables
+start_date = ""
+end_date = ""
+string_date = ""
+string_datetime = ""
 
 if __name__ == "__main__":
     db.connect()
+    start_date, end_date, string_date, string_datetime = dateHandler.getDate()    
